a94eaef05d1026c7031725e629294eec7824db62
Taeyeong Kim <xodud7737@gmail.com>
2024-02-13 14:11:11 +0900

    not Finished

---
+++ 백준-미해결/1789.py
@@ -0,0 +1,31 @@
+# 서로 다른 자연수의 Sum
+# Sum given
+
+# Q. 가장 많이 조합할 수 있는 자연수의 case에서
+# case가 가지는 자연수의 수?
+
+# not in 사용 가능!
+
+# 
+
+
+num = int(input())
+temp = num
+counter =[]
+i=1
+
+for k in range(temp):
+    if num-i >= 0 and (num-i) not in counter:
+        counter.append(i)
+        num = num-i
+    if num-i< 0:
+        break
+    i = i+1
+
+print(len(counter))
+
+
+    
+    
+
+
